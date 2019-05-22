0a024498f9513bbd05536c946f8e2e2c5aa0385f
sizigia <cumbresdigitales@gmail.com>
2019-05-22 20:00:26 -0300

    solution

--- Codewars/morse.js
+++ Codewars/morse.js
@@ -1,5 +1,6 @@
-decodeMorse = function(morseCode){
-  MORSE_CODE['.--']
+decodeMorse = function (morseCode) {
+  const morseLetter = (letter) => MORSE_CODE[letter] || letter;
+  return morseCode.trim().split(' ').map(morseLetter).join('').replace(/\s+/g, ' ');
 }
 
 console.log(decodeMorse('.... . -.--   .--- ..- -.. .'));
