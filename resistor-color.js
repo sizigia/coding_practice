96c26dde62d0c60d3f655c55ccdfbd0c990a6e34
sizigia <cumbresdigitales@gmail.com>
2019-05-29 09:50:53 -0300

    solution - exercism

---
+++ resistor-color.js
@@ -0,0 +1,17 @@
+function colorCode(color) {
+    const resistor = {
+        black: 0,
+        brown: 1,
+        red: 2,
+        orange: 3,
+        yellow: 4,
+        green: 5,
+        blue: 6,
+        violet: 7,
+        grey: 8,
+        white: 9,
+    }
+    return resistor[color];
+}
+
+console.log(colorCode("black"));
