b8535d2e6f068718360e2583f11c67018f52221d
sizigia <cumbresdigitales@gmail.com>
2019-07-31 17:53:49 -0400

    solution

--- intermediateAlgoScript/dnaPairing.js
+++ intermediateAlgoScript/dnaPairing.js
@@ -1,19 +1,18 @@
 function pairElement(str) {
-    let at = 'AT',
-        gc = 'GC';
-    return str.split('').map(x => {
-        if (at.includes(x)) {
-            if (at.indexOf(x) == 1) {
-                return at.split('').reverse();
+    let base = ['AT', 'CG'];
+    let paired = [];
+
+    str.split('').map(bUnit => {
+        base.map(x => {
+            if (x.indexOf(bUnit) == 0) {
+                paired.push(x.split(''));
             }
-            return at.split('');
-        } else {
-            if (gc.indexOf(x) == 1) {
-                return gc.split('').reverse();
+            else if (x.indexOf(bUnit) == 1) {
+                paired.push(x.split('').reverse())
             }
-            return gc.split('');
-        }
+        })
     });
+    return paired;
 }
 
 console.log(pairElement("GCG"));
