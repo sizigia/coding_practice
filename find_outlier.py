0047859d62d009c0224a8fb94fb25c5d3f92b8d5
Faustina Maria <faustina.giaquinta@gmail.com>
2020-05-07 10:26:56 -0400

    kata v1

---
+++ find_outlier.py
@@ -0,0 +1,29 @@
+"""
+https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/python
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+Write a method that takes the array as an argument and returns this "outlier" N.
+
+Examples
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+"""
+
+
+def find_outlier(integers):
+    import random
+    import numpy as np
+
+    r1 = 0
+    r2 = 1
+
+    while r1 != r2:
+        r1 = random.choice(integers) % 2
+        r2 = random.choice(integers) % 2
+
+    if r1:
+        return [x for x in integers if x % 2 == 0][0]
+    else:
+        return [x for x in integers if x % 2 != 0][0]
