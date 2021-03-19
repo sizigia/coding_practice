7a7ef2a9e69d6c5081efa3804bb90de326ae3f03
Faustina Maria <faustina.giaquinta@gmail.com>
2021-03-19 15:25:20 -0500

    created Class and passed all tests except reset_name, there's a problem with the seed

--- exercism/python/robot-name/robot_name.py
+++ exercism/python/robot-name/robot_name.py
@@ -1,3 +1,13 @@
+from string import ascii_uppercase, digits
+import random
+
+
 class Robot:
     def __init__(self):
-        pass
+        uppercase_ = random.choices(ascii_uppercase, k=2)
+        digits_ = random.choices(digits, k=3)
+
+        self.name = ''.join(uppercase_ + digits_)
+
+    def reset(self):
+        self.__init__()
