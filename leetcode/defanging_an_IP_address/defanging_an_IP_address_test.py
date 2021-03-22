5599bc3162d5671ce4b6a771f699b541f1d55489
Faustina Maria <faustina.giaquinta@gmail.com>
2021-03-22 17:34:39 -0500

    fixed Solution instantiation

--- leetcode/defanging_an_IP_address/defanging_an_IP_address_test.py
+++ leetcode/defanging_an_IP_address/defanging_an_IP_address_test.py
@@ -8,13 +8,13 @@ class DefangingIPTest(unittest.TestCase):
     def test_simple_IP_address(self):
         address = "1.1.1.1"
 
-        self.assertEqual(Solution.defangIPaddr(address),
+        self.assertEqual(Solution().defangIPaddr(address),
                          "1[.]1[.]1[.]1")
 
     def test_complex_IP_address(self):
         address = "255.100.50.0"
 
-        self.assertEqual(Solution.defangIPaddr(address),
+        self.assertEqual(Solution().defangIPaddr(address),
                          "255[.]100[.]50[.]0")
 
 
