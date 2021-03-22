9651b6b5ab3653b9f206cfd17938cc08e244f863
Faustina Maria <faustina.giaquinta@gmail.com>
2021-03-22 16:27:58 -0500

    adding solution

--- leetcode/running_sum_of_1d_array/running_sum_of_1d_array.py
+++ leetcode/running_sum_of_1d_array/running_sum_of_1d_array.py
@@ -1,4 +1,7 @@
-
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        pass
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return [sum(nums[:i]) for i in range(1, len(nums) + 1)]
