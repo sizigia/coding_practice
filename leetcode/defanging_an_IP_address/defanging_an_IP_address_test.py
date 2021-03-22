c5e36e9fe4856ccab6f1e470094f6d3120ef4a8b
Faustina Maria <faustina.giaquinta@gmail.com>
2021-03-22 16:56:46 -0500

    adding problem statement, unit test,and code file

---
+++ leetcode/defanging_an_IP_address/defanging_an_IP_address_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from defanging_an_IP_address import Solution
+
+
+class DefangingIPTest(unittest.TestCase):
+
+    def test_simple_IP_address(self):
+        address = "1.1.1.1"
+
+        self.assertEqual(Solution.defangIPaddr(address),
+                         "1[.]1[.]1[.]1")
+
+    def test_complex_IP_address(self):
+        address = "255.100.50.0"
+
+        self.assertEqual(Solution.defangIPaddr(address),
+                         "255[.]100[.]50[.]0")
+
+
+if __name__ == '__main__':
+    unittest.main()
