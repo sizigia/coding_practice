2b51725f9a21ad488221ffb5faff0ae2a77d1990
Faustina Maria <faustina.giaquinta@gmail.com>
2022-08-29 11:27:13 +0200

    completed docstring

--- exercism/python/gigasecond/gigasecond.py
+++ exercism/python/gigasecond/gigasecond.py
@@ -4,13 +4,11 @@ def add(moment):
     """Determine the moment that would
     be after a gigasecond has passed.
     
-    :param moment: datetime - moment in time 
-    :return: - 
+    :param moment: datetime - moment in time.
+    :return: int - moment after a gigasecond has passed.
 
     Function that takes a moment in time and determines
     the moment that would be after a gigasecond has passed.
     """
-    
-
 
     return moment + timedelta(seconds=1000000000)
