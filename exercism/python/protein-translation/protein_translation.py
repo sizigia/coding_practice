fbd7258fb3998c0c6dab51e7298c18d8867b09b3
Faustina Maria <faustina.giaquinta@gmail.com>
2022-08-29 09:24:41 +0200

    added solution

--- exercism/python/protein-translation/protein_translation.py
+++ exercism/python/protein-translation/protein_translation.py
@@ -1,2 +1,32 @@
+CODONS = {'AUG': 'Methionine',
+          'UUU': 'Phenylalanine',
+          'UUC': 'Phenylalanine',
+          'UUA': 'Leucine',
+          'UUG': 'Leucine',
+          'UCU': 'Serine',
+          'UCC': 'Serine',
+          'UCA': 'Serine',
+          'UCG': 'Serine',
+          'UAU': 'Tyrosine',
+          'UAC': 'Tyrosine',
+          'UGU': 'Cysteine',
+          'UGC': 'Cysteine',
+          'UGG': 'Tryptophan',
+          'UAA': 'STOP',
+          'UAG': 'STOP',
+          'UGA': 'STOP'}
+
+
 def proteins(strand):
-    pass
+    n = 3
+    codons = [(strand[i:i + n]) for i in range(0, len(strand), n)]
+
+    proteins = []
+
+    for codon in codons:
+        if codon in ['UAA', 'UAG', 'UGA']:
+            break
+        else:
+            proteins.append(CODONS[codon])
+
+    return proteins
