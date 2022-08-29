b3cc636aba01108e6bb5bd7c8148c7d66791be31
Faustina Maria <faustina.giaquinta@gmail.com>
2022-08-29 11:25:52 +0200

    added solution

---
+++ exercism/python/gigasecond/gigasecond.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+
+def add(moment):
+    """Determine the moment that would
+    be after a gigasecond has passed.
+    
+    :param moment: datetime - moment in time 
+    :return: - 
+
+    Function that takes a moment in time and determines
+    the moment that would be after a gigasecond has passed.
+    """
+    
+
+
+    return moment + timedelta(seconds=1000000000)
