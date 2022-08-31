314fbd243b88f57c6adddcaeb6e58f358e049af8
Faustina Maria <faustina.giaquinta@gmail.com>
2022-08-31 14:26:17 +0200

    added docstrings to Currency Exchange

--- exercism/python/currency-exchange/exchange.py
+++ exercism/python/currency-exchange/exchange.py
@@ -1,5 +1,5 @@
 def exchange_money(budget, exchange_rate):
-    """
+    """Calculate the value of the exchanged currency.
 
     :param budget: float - amount of money you are planning to exchange.
     :param exchange_rate: float - unit value of the foreign currency.
@@ -10,7 +10,7 @@ def exchange_money(budget, exchange_rate):
 
 
 def get_change(budget, exchanging_value):
-    """
+    """Calculate the amount of money that is left from the budget after exchanging.
 
     :param budget: float - amount of money you own.
     :param exchanging_value: float - amount of your money you want to exchange now.
@@ -21,7 +21,8 @@ def get_change(budget, exchanging_value):
 
 
 def get_value_of_bills(denomination, number_of_bills):
-    """
+    """Calculate the total value of the bills (excluding fractional amounts)
+    the booth would give back.
 
     :param denomination: int - the value of a bill.
     :param number_of_bills: int - amount of bills you received.
@@ -32,7 +33,8 @@ def get_value_of_bills(denomination, number_of_bills):
 
 
 def get_number_of_bills(budget, denomination):
-    """
+    """Calculate the number of new currency bills that you can receive within the
+    given budget.
 
     :param budget: float - the amount of money you are planning to exchange.
     :param denomination: int - the value of a single bill.
@@ -43,7 +45,8 @@ def get_number_of_bills(budget, denomination):
 
 
 def get_leftover_of_bills(budget, denomination):
-    """
+    """Calculate the leftover amount that cannot be exchanged from your budget given the denomination
+    of bills.
 
     :param budget: float - the amount of money you are planning to exchange.
     :param denomination: int - the value of a single bill.
@@ -54,7 +57,7 @@ def get_leftover_of_bills(budget, denomination):
 
 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
-    """
+    """Calculate the maximum value of the new currency after exchange rate plus the spread.
 
     :param budget: float - the amount of your money you are planning to exchange.
     :param exchange_rate: float - the unit value of the foreign currency.
