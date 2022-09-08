5cee50c0d7948bd2cd3d648fc64ffdc92c2bc751
Faustina Maria <faustina.giaquinta@gmail.com>
2022-09-08 11:24:54 +0200

    adding solution for Grains exercism exercise

--- exercism/python/grains/grains.py
+++ exercism/python/grains/grains.py
@@ -1,6 +1,13 @@
 def square(number):
-    pass
+    if (number <= 0) or (number > 64):
+        raise ValueError('square must be between 1 and 64')
 
+    return 2 ** (number - 1)
 
 def total():
-    pass
+    total_number_grains = 0
+
+    for square_number in range(1, 65):
+        total_number_grains += square(square_number)
+
+    return total_number_grains
