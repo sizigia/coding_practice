620715146da2ea96b71ca03332f529df44b394f3
Faustina Maria <faustina.giaquinta@gmail.com>
2022-09-12 21:29:05 +0200

    adding solution for Collatz Conjecture exercism exercise

--- exercism/python/collatz-conjecture/collatz_conjecture.py
+++ exercism/python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,18 @@
+def divide_number(number):
+    divs = {
+        0: lambda number: number / 2,
+        1: lambda number: 3 * number + 1,
+    }
+    
+    return divs[number % 2](number) 
+
 def steps(number):
-    pass
+    step = 0
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    while number != 1:
+        number = divide_number(number)
+        step += 1
+    
+    return step
