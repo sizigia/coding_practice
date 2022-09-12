24148b33fb0029d58af3095f334872d0c930c6c4
Faustina Maria <faustina.giaquinta@gmail.com>
2022-09-12 19:59:26 +0200

    adding second iteration, after refactoring solution for Yacht exercism exercise

--- exercism/python/yacht/yacht.py
+++ exercism/python/yacht/yacht.py
@@ -1,19 +1,38 @@
 # Score categories.
 # Change the values as you see fit.
-YACHT = lambda dice: 50 if dice.count(dice[0]) == 5 else 0
-ONES = 1
-TWOS = 2
-THREES = 3
-FOURS = 4
-FIVES = 5
-SIXES = 6
-FULL_HOUSE = lambda max_key, min_key, dice_values: 3 * max_key + 2 * min_key if (max(dice_values.values()), min(dice_values.values())) == (3, 2) else 0
-FOUR_OF_A_KIND = lambda key, dice_values: 4 * key if max(dice_values.values()) >= 4 else 0
-LITTLE_STRAIGHT = lambda dice: 30 if dice == [1, 2, 3, 4, 5] else 0
-BIG_STRAIGHT = lambda dice: 30 if dice == [2, 3, 4, 5, 6] else 0
-CHOICE = lambda dice: sum(dice)
+YACHT = (lambda dice: 50 if dice.count(dice[0]) == 5 else 0)
+ONES = (lambda dice: digits(dice, 1))
+TWOS = (lambda dice: digits(dice, 2))
+THREES = (lambda dice: digits(dice, 3))
+FOURS = (lambda dice: digits(dice, 4))
+FIVES = (lambda dice: digits(dice, 5))
+SIXES = (lambda dice: digits(dice, 6))
+FULL_HOUSE = (lambda dice: full_house(dice))
+FOUR_OF_A_KIND = (lambda dice: four_of_a_kind(dice))
+LITTLE_STRAIGHT = (lambda dice: 30 if sorted(dice) == [1, 2, 3, 4, 5] else 0)
+BIG_STRAIGHT = (lambda dice: 30 if sorted(dice) == [2, 3, 4, 5, 6] else 0)
+CHOICE = (lambda dice: sum(dice))
 
+def dice_values(dice):
+    return {num: dice.count(num) for num in dice}
 
+def digits(dice, score):
+    return dice.count(score) * score
+
+def full_house(dice):
+    dice_count = dice_values(dice)
+    max_key = max(dice_count, key=dice_count.get)
+    min_key = min(dice_count, key=dice_count.get)
+
+    print(sorted(dice_count.values()))
+    return 3 * max_key + 2 * min_key if sorted(dice_count.values()) == [2, 3] else 0
+
+def four_of_a_kind(dice):
+    dice_count = dice_values(dice)
+    key = max(dice_count, key=dice_count.get)
+
+    return 4 * key if max(dice_count.values()) >= 4 else 0
+    
 def score(dice, category):
     """Calculate the score achieved for the given dice and category, according with the Yacht dice game.
 
@@ -22,21 +41,4 @@ def score(dice, category):
     :return: int - indicating the score for that category
     """
 
-    dice_values = {num: dice.count(num) for num in dice}
-
-    if category in [ONES, TWOS, THREES, FOURS, FIVES, SIXES]:
-        return dice.count(category) * category
-    elif category == YACHT:
-        return YACHT(dice)
-    elif category == FULL_HOUSE:
-        max_key = max(dice_values, key=dice_values.get)
-        min_key = min(dice_values, key=dice_values.get)
-        return FULL_HOUSE(max_key, min_key, dice_values)
-    elif category == FOUR_OF_A_KIND:
-        key = max(dice_values, key=dice_values.get)
-        return FOUR_OF_A_KIND(key, dice_values)
-    elif category in [LITTLE_STRAIGHT, BIG_STRAIGHT]:
-        dice.sort()
-        return category(dice)
-    elif category == CHOICE:
-        return CHOICE(dice)
+    return category(dice)
