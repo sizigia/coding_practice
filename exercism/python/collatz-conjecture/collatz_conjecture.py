5296cb323b14bad55f2e8207ecc05c5ab93edde2
Faustina Maria <faustina.giaquinta@gmail.com>
2022-09-14 16:09:34 +0200

    adding docstring and moved initialization variable -step- for Collatz Conjecture exercism exercise

--- exercism/python/collatz-conjecture/collatz_conjecture.py
+++ exercism/python/collatz-conjecture/collatz_conjecture.py
@@ -7,10 +7,15 @@ def divide_number(number):
     return divs[number % 2](number) 
 
 def steps(number):
-    step = 0
+    """Determine the number of steps that take to reduce a given number to 1 by diving it by 2 if even; or multiplying it by 3 and adding 1, if odd. 
+
+    :param number: int - number to be reduce to 1
+    :return step: int - represents the number of steps required to reduce number to 1 
+    """
     if number <= 0:
         raise ValueError("Only positive integers are allowed")
 
+    step = 0
     while number != 1:
         number = divide_number(number)
         step += 1
