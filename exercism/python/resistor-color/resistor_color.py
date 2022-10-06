a2732686f1393c2226173f385cc090d2e1c78aeb
Faustina Maria <faustina.giaquinta@gmail.com>
2022-10-06 15:09:25 +0200

    adding solution for Resistor Color exercism exercise

--- exercism/python/resistor-color/resistor_color.py
+++ exercism/python/resistor-color/resistor_color.py
@@ -1,6 +1,28 @@
-def color_code(color):
-    pass
+def color_code(color) -> int:
+    """Look up the numerical value associated with a particular
+    encoding color band.
+
+    :param color: str - color to look up
+    :return: int - numerical value associated with given color
+    """
+    return colors().index(color)
 
 
-def colors():
-    pass
+def colors() -> list:
+    """List the encoding band colors.
+
+    :return: list - list of different band colors
+    """
+    encoding = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
+    return encoding
