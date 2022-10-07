4e8adbc4d307c57d3babb9ba1777ac7e20b7565a
Faustina Maria <faustina.giaquinta@gmail.com>
2022-10-07 07:19:41 +0200

    adding Resistor Color Duo exercism exercise solution

--- exercism/python/resistor-color-duo/resistor_color_duo.py
+++ exercism/python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,23 @@
-def value(colors):
-    pass
+def value(colors) -> int:
+    """Return value of dual resistor.
+
+    :param colors: list - list of resistor colors
+    :return: int - dual resistor value
+    """
+    encoding = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
+
+    value_temp = "{}{}"
+    value_colors = [encoding.index(color) for color in colors[:2]]
+
+    return int(value_temp.format(*value_colors))
