1ccc7c0a38ad0192589f93867f72a9c733548970
Faustina Maria <faustina.giaquinta@gmail.com>
2022-10-08 09:17:34 +0200

    adding Black Jack exercism exercise solution

--- exercism/python/black-jack/black_jack.py
+++ exercism/python/black-jack/black_jack.py
@@ -5,7 +5,7 @@ How to play blackjack:    https://bicyclecards.com/how-to-play/blackjack/
 """
 
 
-def value_of_card(card):
+def value_of_card(card) -> int:
     """Determine the scoring value of a card.
 
     :param card: str - given card.
@@ -15,11 +15,20 @@ def value_of_card(card):
     2.  'A' (ace card) = 1
     3.  '2' - '10' = numerical value.
     """
+    values = {
+        'J': 10,
+        'Q': 10,
+        'K': 10,
+        'A': 1,
+    }
 
-    pass
+    if card in ['J', 'Q', 'K', 'A']:
+        return values[card]
+
+    return int(card)
 
 
-def higher_card(card_one, card_two):
+def higher_card(card_one, card_two) -> str or tuple:
     """Determine which card has a higher value in the hand.
 
     :param card_one, card_two: str - cards dealt in hand.  See below for values.
@@ -29,11 +38,16 @@ def higher_card(card_one, card_two):
     2.  'A' (ace card) = 1
     3.  '2' - '10' = numerical value.
     """
+    hand = [card_one, card_two]
+    values = [value_of_card(card) for card in hand]
 
-    pass
+    if card_one == card_two or values[0] == values[1]:
+        return (card_one, card_two)
+
+    return hand[values.index(max(values))]
 
 
-def value_of_ace(card_one, card_two):
+def value_of_ace(card_one, card_two) -> int:
     """Calculate the most advantageous value for the ace card.
 
     :param card_one, card_two: str - card dealt. See below for values.
@@ -43,11 +57,13 @@ def value_of_ace(card_one, card_two):
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
     """
+    change_ace_val = lambda card: 11 if card == 'A' else value_of_card(card)
+    values = [change_ace_val(card) for card in (card_one, card_two)]
 
-    pass
+    return 11 if sum(values) + 11 <= 21 else 1
 
 
-def is_blackjack(card_one, card_two):
+def is_blackjack(card_one, card_two) -> bool:
     """Determine if the hand is a 'natural' or 'blackjack'.
 
     :param card_one, card_two: str - card dealt. See below for values.
@@ -57,25 +73,29 @@ def is_blackjack(card_one, card_two):
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
     """
+    change_ace_val = lambda card: 11 if card == 'A' else value_of_card(card)
+    values = [change_ace_val(card) for card in (card_one, card_two)]
 
-    pass
+    return sum(values) == 21
 
 
-def can_split_pairs(card_one, card_two):
+def can_split_pairs(card_one, card_two) -> bool:
     """Determine if a player can split their hand into two hands.
 
     :param card_one, card_two: str - cards dealt.
     :return: bool - can the hand be split into two pairs? (i.e. cards are of the same value).
     """
+    values = [value_of_card(card) for card in (card_one, card_two)]
 
-    pass
+    return all(card == values[0] for card in values)
 
 
-def can_double_down(card_one, card_two):
+def can_double_down(card_one, card_two) -> bool:
     """Determine if a blackjack player can place a double down bet.
 
     :param card_one, card_two: str - first and second cards in hand.
     :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
     """
+    values = [value_of_card(card) for card in (card_one, card_two)]
 
-    pass
+    return sum(values) >= 9 and sum(values) <= 11
